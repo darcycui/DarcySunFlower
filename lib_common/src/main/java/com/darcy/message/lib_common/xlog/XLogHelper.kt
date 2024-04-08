90a3929eeb27b07383cf2e022dfac0ccca87e792
cuiqianbo <darcycui>
2024-04-08 09:01:26 +0800

    feat: only show debug log

--- lib_common/src/main/java/com/darcy/message/lib_common/xlog/XLogHelper.kt
+++ lib_common/src/main/java/com/darcy/message/lib_common/xlog/XLogHelper.kt
@@ -1,13 +1,14 @@
 package com.darcy.message.lib_common.xlog
 
 import android.content.Context
+import com.darcy.message.lib_common.BuildConfig
 import com.elvishew.xlog.LogLevel
 import com.elvishew.xlog.XLog
 
 object XLogHelper {
     fun init(context: Context) {
         // darcyRefactor library BuildConfig can not touch ???
-//        XLog.init(if (BuildConfig.DEBUG) LogLevel.ALL else LogLevel.NONE)
-        XLog.init(LogLevel.ALL)
+        XLog.init(if (BuildConfig.DEBUG) LogLevel.ALL else LogLevel.NONE)
+//        XLog.init(LogLevel.ALL)
     }
 }
