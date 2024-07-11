725a62887a9162c8ef8cdba8d3527423f5eb552d
TOM <tom@jerry.com>
2024-07-11 11:04:12 +0800

    feat: add ListPrefrence changed listener

--- app/src/main/java/com/darcy/message/sunflower/ui/settings/fragment/SettingsFragment.kt
+++ app/src/main/java/com/darcy/message/sunflower/ui/settings/fragment/SettingsFragment.kt
@@ -1,8 +1,11 @@
 package com.darcy.message.sunflower.ui.settings.fragment
 
 import android.os.Bundle
+import androidx.preference.ListPreference
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.PreferenceManager
+import com.darcy.message.lib_common.exts.logD
 import com.darcy.message.lib_ui.exts.toasts
 import com.darcy.message.sunflower.R
 
@@ -17,6 +20,17 @@ class SettingsFragment : PreferenceFragmentCompat() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val manager: PreferenceManager = preferenceManager
+
+        // 根据android:key中指定的名称（相当于id）来获取首选项
+        val listPreference: ListPreference? =
+            manager.findPreference<ListPreference>(getString(R.string.app_pref_general_key_test_list1))
+        logD(message = "存储的值为:${listPreference?.value}")
+        listPreference?.onPreferenceChangeListener =
+            Preference.OnPreferenceChangeListener { preference, newValue ->
+                logD(message = "存储的值为:$newValue")
+                true
+            }
 
     }
 
@@ -30,6 +44,7 @@ class SettingsFragment : PreferenceFragmentCompat() {
                 // TODO
                 toasts("test item1")
             }
+
             getString(R.string.app_pref_general_key_test_item2) -> {
                 // TODO
                 toasts("test item2")
