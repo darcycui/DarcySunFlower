9f1c7d6ee854a2024f708b71b01f711762eec2b6
Tom_docker <Tom_docker@jerry.com>
2025-04-23 09:47:42 +0800

    feat: use repo1 maven repository to replace mavenCentral()

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,6 +6,15 @@ pluginManagement {
 
     repositories {
         maven ("https://maven.aliyun.com/repository/public")
+        // 使用 repo1 maven 代替 mavenCentral, 注释mavenCentral()
+        maven("https://repo1.maven.org/maven2") {
+            // 禁用元数据重定向 强制只从此仓库解析
+            metadataSources {
+                mavenPom()
+                artifact()
+                ignoreGradleMetadataRedirection()
+            }
+        }
         maven("https://maven.aliyun.com/repository/google")
         maven("https://maven.aliyun.com/repository/jcenter")
         maven("https://maven.aliyun.com/repository/gradle-plugin")
@@ -17,7 +26,8 @@ pluginManagement {
                 includeGroupByRegex("androidx.*")
             }
         }
-        mavenCentral()
+        google()
+//        mavenCentral()
         gradlePluginPortal()
     }
 }
@@ -25,14 +35,22 @@ dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         maven ("https://maven.aliyun.com/repository/public")
+        // 使用 repo1 maven 代替 mavenCentral, 注释mavenCentral()
+        maven("https://repo1.maven.org/maven2") {
+            // 禁用元数据重定向 强制只从此仓库解析
+            metadataSources {
+                mavenPom()
+                artifact()
+                ignoreGradleMetadataRedirection()
+            }
+        }
         maven("https://maven.aliyun.com/repository/google")
         maven("https://maven.aliyun.com/repository/jcenter")
         maven("https://maven.aliyun.com/repository/gradle-plugin")
-        maven("https://jitpack.io")
         // umeng
         maven ("https://repo1.maven.org/maven2/")
         google()
-        mavenCentral()
+//        mavenCentral()
     }
 }
 
